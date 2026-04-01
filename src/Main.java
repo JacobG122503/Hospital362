ef9af467e34b6522ac8b8ec1fedcb45f85cd1698
jacobg <jaybob1225@gmail.com>
2026-04-01 12:39:16 -0500

    Basic setup

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J\033[3J");
+        System.out.flush();
+
+        String[] banner = {
+            " _   _                 _ _        _       _____  ____  _____ ",
+            "| | | |               (_) |      | |     |____ |/ ___|/ __  \\",
+            "| |_| | ___  ___ _ __  _| |_ __ _| |______   / / /___ `' / /'",
+            "|  _  |/ _ \\/ __| '_ \\| | __/ _` | |______|  \\ \\ ___ \\  / /  ",
+            "| | | | (_) \\__ \\ |_) | | || (_| | |     .___/ / \\_/ |./ /___",
+            "\\_| |_/\\___/|___/ .__/|_|\\__\\__,_|_|     \\____/\\_____/\\_____/",
+            "                | |                                          ",
+            "                |_|                                          "
+        };
+
+        System.out.println();
+        for (String line : banner) {
+            System.out.println(line);
+            sleep(120);
+        }
+        System.out.println();
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
